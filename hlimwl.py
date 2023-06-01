5caae677ee7f19ae3baef4f36e2eb217383bdd10
yabobay <yabobayyabobay@gmail.com>
2023-06-01 11:23:02 +0300

    also print out number of videos in the playlist
    i named a variable 物件 because the word object is a keyword

--- hlimwl.py
+++ hlimwl.py
@@ -3,34 +3,7 @@ from ytdl_object import ytdl
 import argparse
 from sys import argv
 
-def duration(video): # can be playlist also
-    vid = ytdl.extract_info(video, download=False)
-    if 'duration' in vid.keys(): # its a single video
-        dur = vid['duration']
-    elif 'entries' in vid.keys(): # it's a playlist
-        dur = 0
-        for i in vid['entries']:
-            try:
-                dur += i['duration']
-            except TypeError:
-                # its a private video i guess :P
-                pass
-    return dur
-
-def printPlaylistDuration(playlist=':ytwatchlater'):
-    try:
-        length = duration(playlist)
-        plength = str(timedelta(seconds=length))
-        message = f"Your Watch Later playlist is {plength} long."
-        if length > 36000: # 10 hours
-            message += " Yikes!"
-        print(message)
-    except NameError:
-        print("No browser found :(",
-              "You need to login to YouTube in some web browser.",
-              sep="\n")
-
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(prog='HLIMWL',
                                      description='How Long Is My Watch Later?')
     parser.add_argument('playlist',
@@ -40,3 +13,40 @@ if __name__ == '__main__':
     else:
         args = parser.parse_args()
         printPlaylistDuration(args.playlist)
+
+def duration(video): # can be playlist also
+    物件 = {} # it means object
+    vid = ytdl.extract_info(video, download=False)
+    if 'duration' in vid.keys(): # its a single video
+        物件['type'] = 'video'
+        dur = vid['duration']
+    elif 'entries' in vid.keys(): # it's a playlist
+        物件['type'] = 'playlist'
+        物件['videos'] = len(vid['entries'])
+        dur = 0
+        for i in vid['entries']:
+            try:
+                dur += i['duration']
+            except TypeError:
+                # its a private video i guess :P
+                pass
+    物件['duration'] = dur
+    return 物件
+
+def printPlaylistDuration(playlist=':ytwatchlater'):
+    try:
+        pl = duration(playlist)
+        length = pl['duration']
+        pvideos = pl['videos']
+        plength = str(timedelta(seconds=length))
+        message = f"Your playlist contains {pvideos} videos and is {plength} long."
+        if length > 36000: # 10 hours
+            message += " Yikes!"
+        print(message)
+    except NameError:
+        print("No browser found :(",
+              "You need to login to YouTube in some web browser.",
+              sep="\n")
+
+if __name__ == '__main__':
+    main()
