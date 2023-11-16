980fe5c2566b955eb80ad3203ccd07af3ee01d59
yabobay <yabobayyabobay@gmail.com>
2023-11-16 10:53:38 +0200

    Full rewrite! now using official Google APIs. :/

--- hlimwl.py
+++ hlimwl.py
@@ -1,53 +1,73 @@
-from datetime import timedelta
-import ytdl_object
-import argparse
-from sys import argv
+from googleapiclient.discovery import build
+import re
+from math import ceil, floor
+import time
+from progress.bar import Bar
+from progress.spinner import Spinner
 
-def main():
-    parser = argparse.ArgumentParser(prog='HLIMWL',
-                                     description='How Long Is My Watch Later?')
-    parser.add_argument('-p', help='playlist to check the length of',
-                        metavar='playlist', default=':ytwatchlater',
-                        required=False)
-    parser.add_argument('-v', help='verbose mode', action='store_true')
-    args = parser.parse_args()
-    global ytdl # this script is garbage
-    ytdl = ytdl_object.makeYtdlObject(args.v)
-    printPlaylistDuration(args.p)
+# it's pretty hard to make an empty playlist on purpose, and even if
+# you did, you're probably not trying to measure it's duration. so if
+# we get this, it means there's probably something wrong with the
+# playlist.
+class EmptyPlaylistError(Exception): pass
 
-def duration(video): # can be playlist also
-    物件 = {} # it means object
-    vid = ytdl.extract_info(video, download=False)
-    if 'duration' in vid.keys(): # its a single video
-        物件['type'] = 'video'
-        dur = vid['duration']
-    elif 'entries' in vid.keys(): # it's a playlist
-        物件['type'] = 'playlist'
-        物件['videos'] = len(vid['entries'])
-        dur = 0
-        for i in vid['entries']:
-            try:
-                dur += i['duration']
-            except TypeError:
-                # its a private video i guess :P
-                pass
-    物件['duration'] = dur
-    return 物件
+def videoLength(video_id):
+    # TODO: add converting from URL
+    length = youtube\
+        .videos().list(part='contentDetails', id=video_id).execute()\
+        ['items'][0]['contentDetails']['duration']
+    conversions = {"D": 86400, "H": 3600, "M": 60, "S": 1}
+    return sum([int(i[:-1]) * conversions[i[-1]]
+                for i in re.compile(r"\d+[DHMS]").findall(length)])
 
-def printPlaylistDuration(playlist):
+def playlistVids(playlist_id):
+    if loud: spinner = Spinner("Counting vids... ")
+    vids = []
+    page = ''
+    while True:
+        result = youtube.playlistItems()\
+                        .list(part='snippet,status',
+                              playlistId=playlist_id,
+                              pageToken=page)\
+                        .execute()
+        if result['items'] == []:
+            raise EmptyPlaylistError
+        vids += [vid['snippet']['resourceId']['videoId'] for vid in result['items']
+                 if vid['status']['privacyStatus'] != "private"]
+        try:
+            page = result['nextPageToken']
+        except KeyError:
+            break
+        if loud: spinner.next()
+    if loud: print('', len(vids))
+    return vids
+
+def playlistLength(playlist_id):
+    vids = playlistVids(playlist_id)
+    if loud: bar = Bar("Getting length of vids...", max=len(vids))
+    seconds = 0
+    for vid in vids:
+        seconds += videoLength(vid)
+        if loud: bar.next()
+    if loud: print()
+    return seconds
+
+def formatTime(seconds):
+    s = time.strftime('%H:%M:%S', time.gmtime(seconds))
+    days = floor(seconds / 86400)
+    if days:
+        s = f"{days} days, {s}"
+    return s
+
+loud = __name__ == "__main__"
+
+_key = 'AIzaSyBozcYO6eF75fXGXVpGD_-DfzgmwI7wa8o'
+youtube = build('youtube', 'v3', developerKey=_key)
+
+if __name__ == "__main__":
     try:
-        pl = duration(playlist)
-        length = pl['duration']
-        pvideos = pl['videos']
-        plength = str(timedelta(seconds=length))
-        message = f"Your playlist contains {pvideos} videos and is {plength} long."
-        if length > 36000: # 10 hours
-            message += " Yikes!"
-        print(message)
-    except NameError:
-        print("No browser found :(",
-              "You need to login to YouTube in some web browser.",
-              sep="\n")
+        print(formatTime(playlistLength('WL')))
+    except EmptyPlaylistError:
+        print("Empty Playlist!\nMaybe the playlist is private?")
 
-if __name__ == '__main__':
-    main()
+youtube.close()
